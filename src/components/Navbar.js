aee524d1cb67171469af870b8f8545fde390b90f
Joybaruah <hoyyun1@gmail.com>
2021-11-18 06:09:23 +0530

    Day 1

---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+      <>
+        <nav className="navbar">
+          <div className="logo">
+            <h2>JOY BARUAH</h2>
+          </div>
+            <ul className="links">
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/">About</a>
+              </li>
+              <li>
+                <a href="/">Contact</a>
+              </li>
+            </ul>
+        </nav>
+      </>
+    );
+}
+
+export default Navbar
